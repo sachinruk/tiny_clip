e4d27c866dd707afa17f0d89b1fc782fd2485d4b
Sachin Abeywardana <sachin.abeywardana@gmail.com>
2024-09-08 18:26:33 +1000

    Added cc3m download dockerfile

--- data/download_cc3m.sh
+++ data/download_cc3m.sh
@@ -32,6 +32,5 @@ done
 
 sed -i '1s/^/caption\turl\n/' ./GCC-training.tsv
 img2dataset --url_list ./GCC-training.tsv --input_format "tsv"\
-         --url_col "url" --caption_col "caption" --output_format webdataset\
-           --output_folder cc3m --processes_count 16 --thread_count 64 --image_size 256\
-             --enable_wandb True
+        --url_col "url" --caption_col "caption" --output_format webdataset\
+        --output_folder cc3m --processes_count 16 --thread_count 64 --image_size 256
